7a982c78cf251225d422976d9c8c299cfd8b4b90
Watcher <tobyh808@gmail.com>
2025-03-12 13:32:48 +0000

    Added experiment files

---
+++ Script/Node.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int nodeID;
+    public List<GameObject> neighboringNodes;
+    public bool findNeighboringNodes = false;
+    NodeMapBuilder nodeMapBuilder;
+    public int nodeListID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        findNeighboringNodes = true;
+        nodeMapBuilder = GetComponent<NodeMapBuilder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (findNeighboringNodes == true)
+        {
+            neighboringNodes.Add
+
+                nodeMapBuilder.existingNodes.Add()
+        }
+    }
+}
